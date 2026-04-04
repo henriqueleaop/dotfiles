0f2f57038d7a663ab5a27d3696196914d616c722
liceki <henrique.lp2006@gmail.com>
2026-04-04 14:57:45 -0300

    feat(nvim): add neo-tree.nvim plugin and toggle keymap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -289,6 +289,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    config = function() vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Alternar barra lateral' }) end,
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
